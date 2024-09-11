502f5cf502a0c5499b5c074cff3b90f575fe0c10
IryIndriyanto <iry.indriyanto20@gmail.com>
2024-09-11 19:50:57 +0700

    refactor: delete unused component

--- src/App.tsx
+++ src/App.tsx
@@ -1,89 +1,10 @@
-import { useState, useEffect } from "react";
 import "./App.css";
+import Chatroom from "./components/chatroom";
 
-
-const apiUrl:string = import.meta.env.VITE_API_URL;
-const wsUrl:string = import.meta.env.VITE_WS_URL;
-
-interface Message {
-  id: number;
-  body: string;
-}
-
-const ws = new WebSocket(wsUrl);
 function App() {
-  const [messages, setMessages] = useState<Message[]>([]);
-  const [guid, setGuid] = useState("");
-
-  ws.onopen = () => {
-    console.log("connected to message server");
-    setGuid(Math.random().toString(36).substring(2, 15));
-
-    ws.send(
-      JSON.stringify({
-        command: "subscribe",
-        identifier: JSON.stringify({
-          id: guid,
-          channel: "MessagesChannel",
-        }),
-      })
-    );
-  };
-
-  ws.onmessage = (e) => {
-    const data = JSON.parse(e.data);
-    if (data.type === "ping") return;
-    if (data.type === "welcome") return;
-    if (data.type === "confirm_subscription") return;
-
-    const message = data.message;
-    setMessages([...messages, message]);
-  };
-
-  useEffect(() => {
-    fetchMessages();
-  }, []);
-
-  const fetchMessages = async () => {
-    const response = await fetch(`${apiUrl}/messages`);
-    const data = await response.json();
-    setMessages(data);
-  };
-
-const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-  e.preventDefault();
-  const body = (e.target as HTMLFormElement).message.value;
-  (e.target as HTMLFormElement).message.value = "";
-
-  await fetch(`${apiUrl}/messages`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ body }),
-  });
-};
-
   return (
     <>
-      <div> guid :{guid}</div>
-      <div className="messages" id="messages">
-        {messages.length === 0 ? (
-          <p>No messages yet</p>
-        ) : (
-          messages.map((message) => (
-            <div className="message" key={message.id}>
-              <p>{message.body}</p>
-            </div>
-          ))
-        )}
-      </div>
-      <form onSubmit={handleSubmit}>
-        <input className="messageInput" type="text" name="message" />
-        <button className="messageButton" type="submit">
-          Send
-        </button>
-      </form>
+      <Chatroom />
     </>
   );
 }
